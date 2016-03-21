93d7f23dff43129d2941eff2b007b78988a541fe
Rene Hexel <r.hexel@griffith.edu.au>
2016-03-22 07:27:50 +1000

    Add Fuzi XML parser

---
+++ girparser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  girparser
+//
+//  Created by Rene Hexel on 22/03/2016.
+//  Copyright © 2016 Rene Hexel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
