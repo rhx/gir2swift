4bef8f813b35ed2413ef29e5265dd669eb96550a
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-15 16:21:25 +1000

    Make gir2swift compile with Xcode and external SwiftLibXML module

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "gir2swift",
+    dependencies: [
+        .Package(url: "https://github.com/rhx/SwiftLibXML.git", majorVersion: 1)
+    ]
+)
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+export PKG_CONFIG_PATH=`echo /usr/local/Cellar/libxml2/*/lib/pkgconfig | tr ' ' '\n' | tail -n1`:${PKG_CONFIG_PATH}
+LINKFLAGS=`pkg-config --libs libxml-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+CCFLAGS=`pkg-config --cflags libxml-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
+swift build $CCFLAGS $LINKFLAGS
