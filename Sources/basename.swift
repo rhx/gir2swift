d033ce09ca74876418e4e6badcd81b735ffeeb63
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-18 16:29:45 +1000

    Add a simple basename() for Linux

--- Sources/basename.swift
+++ Sources/basename.swift
@@ -7,14 +7,33 @@
 //
 #if os(Linux)
     import Glibc
+
+    /// just a slash
+    private let slash = "/".utf16.first!
+
+    /// return the base name of a string
+    extension String {
+        var baseName: String {
+            let u = utf16
+            let s = u.startIndex
+            let e = u.endIndex
+            var i = e
+            while i != s {
+                let j = u.index(before: i)
+                if u[j] == slash { return String(u[i..<e]) }
+                i = j
+            }
+            return self
+        }
+    }
 #else
     import Darwin
-#endif
 
 
-/// return the base name of a string
-extension String {
-    var baseName: String {
-        return String(cString: basename(cstring(self)))
+    /// return the base name of a string
+    extension String {
+        var baseName: String {
+            return String(cString: basename(cstring(self)))
+        }
     }
-}
+#endif
