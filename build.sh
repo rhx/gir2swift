b5c60bb958efb927af32aff303655eb1f25d5da3
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-09 13:35:56 +1000

    Regen from package manager

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PA
 export PKG_CONFIG_PATH=`echo /usr/local/Cellar/libxml2/*/lib/pkgconfig | tr ' ' '\n' | tail -n1`:${PKG_CONFIG_PATH}
 LINKFLAGS=`pkg-config --libs libxml-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags libxml-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
-swift build $CCFLAGS $LINKFLAGS
+exec swift build $CCFLAGS $LINKFLAGS "$@"
