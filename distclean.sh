68236875a8e555dde36963f0351d896a57a04172
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-11 09:10:58 +1000

    Remove Package.pins

--- distclean.sh
+++ distclean.sh
@@ -4,4 +4,4 @@
 #
 . ./config.sh
 ./clean.sh
-exec rm -rf Packages Sources/${Module}.swift ${Mod}.xcodeproj
+exec rm -rf Package.pins Packages Sources/${Module}.swift Sources/Swift${Mod}.swift ${Mod}.xcodeproj
