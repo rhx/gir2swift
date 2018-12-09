d8d0d0354440921429cd1985931d5b917c3180a6
Carl Lusty <carl.lusty@griffithuni.edu.au>
2018-12-09 17:24:28 +1000

    Add a fallback TOOLCHAIN path.

--- config.sh
+++ config.sh
@@ -3,8 +3,11 @@
 Mod=gir2swift
 if [ -e /usr/lib/libxml2.2.dylib ]; then
 	TOOLCHAIN=`xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
-        LINKFLAGS='-Xlinker -lxml2.2'
-        CCFLAGS="-Xcc -I${TOOLCHAIN}/usr/include -Xcc -I${TOOLCHAIN}/usr/include/libxml2"
+	if ! [ -e ${TOOLCHAIN} ]; then
+		TOOLCHAIN=`xcode-select -p`/SDKs/MacOSX.sdk
+	fi
+	LINKFLAGS='-Xlinker -lxml2.2'
+	CCFLAGS="-Xcc -I${TOOLCHAIN}/usr/include -Xcc -I${TOOLCHAIN}/usr/include/libxml2"
 else
 	export PKG_CONFIG_PATH=`echo /usr/local/Cellar/libxml2/*/lib/pkgconfig | tr ' ' '\n' | tail -n1`:${PKG_CONFIG_PATH}
 	LINKFLAGS=`pkg-config --libs libxml-2.0 | sed -e 's/  */ /g' -e 's/ *$//' | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' '`
