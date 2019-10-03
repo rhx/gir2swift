160d4fe74bd1812686202d9d80ea7598c05d0c50
Rene Hexel <r.hexel@griffith.edu.au>
2019-10-03 13:53:09 +1000

    Add documentation generator

--- config.sh
+++ config.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 #
 Mod=gir2swift
-if [ -e /usr/lib/libxml2.2.dylib ]; then
+XML_VER=2.2
+MAJOR_VER=2.0
+JAZZY_VER=5.2.0
+export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}:/usr/local/opt/ruby/bin:`echo /usr/local/lib/ruby/gems/*/bin | tr ' ' '\n' | tail -n1`:${PATH}:`echo /var/lib/gems/*/gems/jazzy-*/bin/ | tr ' ' '\n' | tail -n1`:/usr/local/bin"
+if [ -e /usr/lib/libxml${XML_VER}.dylib ]; then
 	TOOLCHAIN=`xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 	if ! [ -e ${TOOLCHAIN} ]; then
 		TOOLCHAIN=`xcode-select -p`/SDKs/MacOSX.sdk
 	fi
-	LINKFLAGS='-Xlinker -lxml2.2'
+	LINKFLAGS="-Xlinker -lxml${XML_VER}"
 	CCFLAGS="-Xcc -I${TOOLCHAIN}/usr/include -Xcc -I${TOOLCHAIN}/usr/include/libxml2"
 else
 	export PKG_CONFIG_PATH=`echo /usr/local/Cellar/libxml2/*/lib/pkgconfig | tr ' ' '\n' | tail -n1`:${PKG_CONFIG_PATH}
-	LINKFLAGS=`pkg-config --libs libxml-2.0 | sed -e 's/  */ /g' -e 's/ *$//' | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' '`
-	CCFLAGS=`pkg-config --cflags libxml-2.0 | sed -e 's/  */ /g' -e 's/ *$//' | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
+	LINKFLAGS=`pkg-config --libs libxml-$MAJOR_VER | sed -e 's/  */ /g' -e 's/ *$//' | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' '`
+	CCFLAGS=`pkg-config --cflags libxml-$MAJOR_VER | sed -e 's/  */ /g' -e 's/ *$//' | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
 fi
---
+++ generate-documentation.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Wrapper around `swift build' that uses pkg-config in config.sh
+# to determine compiler and linker flags
+#
+. ./config.sh
+xmlpkg=`echo .build/checkouts/SwiftLibXML.git-*/Package.swift`
+[ -e $xmlpkg ] || ./generate-wrapper.sh
+if [ -z "$@" ]; then
+    JAZZY_ARGS="--theme fullwidth --author Ren&eacute;&nbsp;Hexel --author_url https://www.ict.griffith.edu.au/~rhexel/ --github_url https://github.com/rhx/$Mod --github-file-prefix https://github.com/rhx/$Mod/tree/master --root-url http://rhx.github.io/$Mod/ --output docs"
+fi
+rm -rf .docs.old
+mv docs .docs.old 2>/dev/null
+sourcekitten doc --spm-module $Mod -- $CCFLAGS $LINKFLAGS > .build/$Mod-doc.json
+jazzy --sourcekitten-sourcefile .build/$Mod-doc.json --clean		\
+      --module-version $JAZZY_VER --module $Mod $JAZZY_ARGS "$@"
