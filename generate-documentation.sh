efbb34e0b94b7c9e7c02ea29bdaec39ec53d86ff
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-09 15:10:16 +1000

    s/master/main/

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -7,7 +7,7 @@
 xmlpkg=`echo .build/checkouts/SwiftLibXML.git-*/Package.swift`
 [ -e $xmlpkg ] || ./generate-wrapper.sh
 if [ -z "$@" ]; then
-    JAZZY_ARGS="--theme fullwidth --author Ren&eacute;&nbsp;Hexel --author_url https://www.ict.griffith.edu.au/~rhexel/ --github_url https://github.com/rhx/$Mod --github-file-prefix https://github.com/rhx/$Mod/tree/master --root-url http://rhx.github.io/$Mod/ --output docs"
+    JAZZY_ARGS="--theme fullwidth --author Ren&eacute;&nbsp;Hexel --author_url https://www.ict.griffith.edu.au/~rhexel/ --github_url https://github.com/rhx/$Mod --github-file-prefix https://github.com/rhx/$Mod/tree/main --root-url http://rhx.github.io/$Mod/ --output docs"
 fi
 rm -rf .docs.old
 mv docs .docs.old 2>/dev/null
