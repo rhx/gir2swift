168905e3885c58faeec6486eeb4a6d221ffbf2fb
Rene Hexel <r.hexel@griffith.edu.au>
2021-05-20 12:38:38 +1000

    Add documentation

--- Sources/libgir2swift/models/Gir2Swift.swift
+++ Sources/libgir2swift/models/Gir2Swift.swift
@@ -10,24 +10,32 @@ import Foundation
 
 /// Structure representing the `gir2swift` executable, including command line arguments
 public struct Gir2Swift: ParsableCommand {
+    /// Produce verbose output if `true`
     @Flag(name: .short, help: "Produce verbose output.")
     var verbose = false
 
+    /// Generate output for everything, including private C types if `true`
     @Flag(name: .short, help: "Disables all filters. Wrappers for all C types will be generated.")
     var allFilesGenerate = false
 
+    /// Create a single output file per class if `true`
     @Flag(name: .short, help: "Create a single .swift file per class.")
     var singleFilePerClass = false
 
+    /// Array of names of pre-parsed `.gir` files.
     @Option(name: .short, help: "Add pre-requisite .gir files to ensure the types in file.gir are known.")
     var prerequisiteGir: [String] = []
 
+    /// Name of the output directory to write generated files to.
+    /// - Note: Writes generated code to `standardOutput` if `nil`
     @Option(name: .short, help: "Specify the output directory to put the generated files into.", transform: String.init(nonEmpty:))
     var outputDirectory: String? = nil
 
+    /// File containing one-off boilerplate code for your module
     @Option(name: .short, help: "Add .swift as the main (hand-crafted) Swift file for your library target.")
     var moduleBoilerPlate: String = ""
 
+    /// The actual, main `.gir` file(s) to process
     @Argument(help: "The .gir metadata files to process.")
     var girFiles: [String]
     
