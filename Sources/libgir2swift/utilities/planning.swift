cf30da8f6b4b8f0468c0d3129356de4121603c44
Mikoláš Stuchlík <stuchlej@gmail.com>
2021-07-24 17:46:18 +0200

    [WIP] Implementation for prerequisities

--- Sources/libgir2swift/utilities/planning.swift
+++ Sources/libgir2swift/utilities/planning.swift
@@ -1,5 +1,7 @@
 import Foundation
+import FoundationNetworking
 import Yams
+import SwiftLibXML
 
 struct Manifest: Codable {
     private enum CodingKeys: String, CodingKey {
@@ -10,33 +12,47 @@ struct Manifest: Codable {
 
     let version: UInt
     let girName: String
-    let pkgConfig: String?
+    let pkgConfig: String
+}
+
+struct GirPackageMetadata {
+    struct Dependency {
+        let name: String
+        let version: String
+
+        var girName: String { name + "-" + version }
+    }
+
+    let pkgName: String
+    let dependency: [Dependency]
 }
 
 struct Plan {
 
     enum Error: Swift.Error {
-        case girNotFound, girPrerequisityNotFound(named: String)
+        case girNotFound(named: String)
+        case girParsingFailed
     }
 
     let girFileToGenerate: URL
     let girFilesToPreload: [URL]
-    let pkgConfigName: String?
+    let pkgConfigName: String
 
-    init(using manifest: URL) throws {
-        let data = try Data.init(contentsOf: manifest)
+    init(using manifestUrl: URL) throws {
+        // From some reason, Data would not provide correct result
+        let data = try String.init(contentsOfFile: manifestUrl.path)
         let manifest = try YAMLDecoder().decode(Manifest.self, from: data)
         
         let girPath = try Plan.searchForGir(
             named: manifest.girName, 
-            pkgConfig: manifest.pkgConfig.flatMap {[$0]} ?? []
+            pkgConfig: [manifest.pkgConfig]
         )
         guard let girPath = girPath else {
-            throw Error.girNotFound
+            throw Error.girNotFound(named: manifest.girName + ".gir" )
         } 
 
         self.girFileToGenerate = girPath
-        self.girFilesToPreload = try Plan.loadPrerequisities(from: girPath)
+        self.girFilesToPreload = try Plan.loadPrerequisities(from: girPath, pkgConfig: manifest.pkgConfig)
         self.pkgConfigName = manifest.pkgConfig
     }
 
@@ -60,12 +76,119 @@ struct Plan {
 
 
         return searchPaths
-            .map { $0.appendingPathComponent(name).appendingPathExtension(".gir") }
-            .first { FileManager.default.fileExists(atPath: $0.path) }
+            .map { $0.appendingPathComponent(name).appendingPathExtension("gir") }
+            .first { 
+                FileManager.default.fileExists(atPath: $0.path) 
+            }
     }
 
-    private static func loadPrerequisities(from gir: URL) throws -> [URL] {
+    private static func loadPrerequisities(from gir: URL, pkgConfig: String) throws -> [URL] {
+        var explored: [String: URL] = [ gir.deletingLastPathComponent().lastPathComponent : gir ]
+        var toExplore: Set<String> = Set(try parsePackageInfo(for: gir).dependency.map(\.girName))
 
-        return []
+        let pkgConfigCandidates: [String]
+        #if os(macOS)
+            pkgConfigCandidates = try getAllPkgConfigDependencies(for: pkgConfig)
+        #else
+            pkgConfigCandidates = []
+        #endif
+
+        
+
+        while true {
+            if toExplore.isEmpty {
+                break
+            }
+
+            let executing = toExplore.removeFirst()
+            guard let url = try searchForGir(named: executing, pkgConfig: pkgConfigCandidates) else {
+                throw Error.girNotFound(named: executing)
+            }
+
+            explored[executing] = url
+
+            let packageInfo = try parsePackageInfo(for: url)
+            packageInfo.dependency.forEach { dependency in
+                if explored[dependency.girName] == nil {
+                    toExplore.insert(dependency.girName)
+                }
+            }
+        }
+
+        return Array(explored.values)
+    }
+
+    private static func getAllPkgConfigDependencies(for package: String) throws -> [String] {
+        var explored: Set<String> = []
+        var toExplore: Set<String> = [package]
+
+        while true {
+            if toExplore.isEmpty {
+                break
+            }
+
+            let executing = toExplore.removeFirst()
+            explored.insert(executing)
+
+            try getPkgConfigDependencies(for: executing).forEach { result in
+                if !explored.contains(result) {
+                    toExplore.insert(result)
+                }
+            }
+        }
+
+        return Array(explored)
+    }
+
+    private static func getPkgConfigDependencies(for package: String) throws -> [String] {
+        guard let output = try executeAndWait("pkg-config", arguments: ["--print-requires", package]) else {
+            return []
+        }
+
+        let dependecyRecord = output.components(separatedBy: .newlines)
+        let packageNames = dependecyRecord.compactMap { line in
+            line.components(separatedBy: .whitespaces).first
+        }
+
+        return packageNames
+    }
+
+    private static func parsePackageInfo(for gir: URL) throws -> GirPackageMetadata {
+        guard let xml = XMLDocument(fromFile: gir.path) else {
+            throw Error.girNotFound(named: gir.lastPathComponent)
+        }
+
+        // @rhx please check this code
+        if let repository = xml.first(where: { $0.name == "repository" }) {
+            let namespaces = repository.namespaces
+
+            guard 
+                let packageName = xml.xpath(
+                    "/gir:repository/gir:package", 
+                    namespaces: namespaces, 
+                    defaultPrefix: "gir"
+                )?.first?.attribute(named: "name") 
+            else { throw Error.girParsingFailed }
+
+            let dependencies = xml.xpath(
+                "/gir:repository/gir:include", 
+                namespaces: namespaces, 
+                defaultPrefix: "gir"
+            )?.lazy.compactMap { node -> GirPackageMetadata.Dependency? in
+                guard
+                    let name = node.attribute(named: "name"),
+                    let version = node.attribute(named: "version")
+                else { return nil }
+                return GirPackageMetadata.Dependency(name: name, version: version)
+            }
+
+            return GirPackageMetadata(
+                pkgName: packageName,
+                dependency: dependencies.flatMap(Array.init(_:)) ?? []
+            )
+
+        }
+
+        throw Error.girParsingFailed
     }
 }
