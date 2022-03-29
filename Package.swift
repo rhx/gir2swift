0183ac68b79422ba15f9009bc7df261f4f8e9e59
Rene Hexel <r.hexel@griffith.edu.au>
2022-03-30 08:18:51 +1000

    Add plugin target (wip)

--- Package.swift
+++ Package.swift
@@ -1,19 +1,21 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let pkgName = "gir2swift"
 let libTarget = "lib\(pkgName)"
+let plugin = "\(pkgName)Plugin"
 
 let package = Package(
     name: pkgName,
     products: [
         .executable(name: pkgName, targets: [pkgName]),
         .library(name: libTarget, targets: [libTarget]),
+        .plugin(name: plugin, targets: [plugin]),
     ],
     dependencies: [ 
-        .package(url: "https://github.com/rhx/SwiftLibXML.git", .branch("main")),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0")
+        .package(url: "https://github.com/rhx/SwiftLibXML.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.1")
     ],
     targets: [
         .target(
@@ -30,5 +32,8 @@ let package = Package(
             ]
         ),
         .testTarget(name: "\(pkgName)Tests", dependencies: [.init(stringLiteral: libTarget)]),
+        .plugin(name: plugin,
+                capability: .buildTool(),
+                dependencies: [.init(stringLiteral: pkgName)]),
     ]
 )
---
+++ Plugins/gir2swiftPlugin/plugin.swift
@@ -0,0 +1,32 @@
+import PackagePlugin
+import Foundation
+
+/// The gir2swift build plugin
+@main
+struct GIR2SwiftPlugin: BuildToolPlugin {
+    /// A Plugin that generates Swift code from a `.gir` FILE
+    /// - Parameters:
+    ///   - context: information about the package for which the plugin is provided
+    ///   - target: the target defined in the package
+    /// - Returns: the commands to run during the build
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+        let gir2swiftManifest = context.package.directory.appending("gir2swift-manifest.yaml")
+        let gir2swiftOutputs = context.pluginWorkDirectory.appending("gir2swift-generated").appending(target.name)
+        try FileManager.default.createDirectory(atPath: gir2swiftOutputs.string, withIntermediateDirectories: true)
+        return [.prebuildCommand(
+            displayName: "Running gir2swift",
+            executable: try context.tool(named: "gir2swift").path,
+            arguments: [
+                "-o", gir2swiftOutputs,
+                "--manifest", "\(gir2swiftManifest)"
+            ],
+            environment: [
+                "PROJECT_DIR": "\(context.package.directory)",
+                "TARGET_NAME": "\(target.name)",
+                "DERIVED_SOURCES_DIR": "\(gir2swiftOutputs)",
+            ],
+            outputFilesDirectory: gir2swiftOutputs)
+        ]
+    }
+}
+
