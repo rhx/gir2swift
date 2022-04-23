d7b4cf4b91ddba32cfef8f68071a02207e5ef03e
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-23 18:50:42 +1000

    Add debug output

--- Sources/gir2swift/main.swift
+++ Sources/gir2swift/main.swift
@@ -6,5 +6,12 @@
 //  Copyright © 2016, 2017, 2018, 2019, 2020, 2021, 2022 Rene Hexel. All rights reserved.
 //
 import libgir2swift
+import Foundation
+
+let fm = FileManager.default
+
+CommandLine.arguments.enumerated().forEach { (i, filename) in
+    fputs("\n\(i): \(filename)", stderr)
+}
 
 Gir2Swift.main()
