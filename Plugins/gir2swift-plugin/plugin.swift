2544cfb959d14c81b729f118618a26a8475b9c4e
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-23 12:32:38 +1000

    Use displayName consistent with SPM output

--- Plugins/gir2swift-plugin/plugin.swift
+++ Plugins/gir2swift-plugin/plugin.swift
@@ -104,7 +104,7 @@ func getGirDirectory(containing girFiles: [String]) throws -> Path {
         })
 
         return [.buildCommand(
-            displayName: "Converting \(girName) for \(target.directory.lastComponent)",
+            displayName: "Converting \(target.directory.lastComponent) \(girName).gir",
             executable: try context.tool(named: "gir2swift").path,
             arguments: arguments,
             inputFiles: inputFiles,
