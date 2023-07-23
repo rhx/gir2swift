b08e63025886bc650771e496af680ecdf7254ee8
Rene Hexel <r.hexel@griffith.edu.au>
2023-07-23 13:45:47 +1000

    Revert "Qualify inner template when namespaced."
    This reverts commit dcaadb703708f1bd6bb9ee5f2f9540a816cbf801.

--- Sources/libgir2swift/models/GirType.swift
+++ Sources/libgir2swift/models/GirType.swift
@@ -574,14 +574,8 @@ func genericReference(named identifier: String? = nil, for name: String, contain
     let refName = String(pureTypeName.hasSuffix("Ref") ? pureTypeName : (pureTypeName + "Ref"))
     let prefixedName = namespace.map { $0 + "." + refName } ?? refName
     let info = decodeIndirection(for: cType)
-    let prefixedContainedType: String
-    if let ns = containedNamespace, !ns.isEmpty {
-        prefixedContainedType = ns + "." + containedTypeName
-    } else {
-        prefixedContainedType = containedTypeName
-    }
-    let templateInstantiation = "<" + prefixedContainedType + ">"
-    let nonOptionalName = String(prefixedName) + templateInstantiation
+    let inner = "<" + containedTypeName + ">"
+    let nonOptionalName = String(prefixedName) + inner
     let suffixedName = nonOptionalName + optionalSuffix
     let maybeType = GIR.namedTypes[suffixedName]?.first { $0.ctype == info.innerType }
     let maybeInnerType = GIR.namedTypes[containedTypeName]?.first
